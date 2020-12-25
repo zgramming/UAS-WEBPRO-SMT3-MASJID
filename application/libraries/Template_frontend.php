21cd61c01f61fa9137bc227dc530e584351b2cd1
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-12-25 19:48:08 +0700

    adding "title" parameter

--- application/libraries/Template_frontend.php
+++ application/libraries/Template_frontend.php
@@ -8,9 +8,10 @@ class Template_frontend
         $this->_ci = &get_instance();
     }
 
-    function display($template, $data = null)
+    function view($template, $data = null, $title = "Default title")
     {
         $data['_content'] = $this->_ci->load->view($template, $data, true);
+        $data['_title'] = $title;
         $this->_ci->load->view('template/frontend/template', $data);
     }
 }
