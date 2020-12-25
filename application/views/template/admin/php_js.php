a1a1db6ddb98367d41022e4fa713760cdd8217d0
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-12-25 19:46:05 +0700

    adding plugin ckeditor

--- application/views/template/admin/php_js.php
+++ application/views/template/admin/php_js.php
@@ -9,16 +9,17 @@
  <script src="<?= base_url('js/sb-admin-2.min.js') ?>"></script>
 
  <!-- Page level plugins -->
- <script src="<?= base_url('vendor/chart.js/Chart.min.js') ?>"></script>
+ <!-- <script src="<?= base_url('vendor/chart.js/Chart.min.js') ?>"></script> -->
 
  <!-- Page level custom scripts -->
- <script src="<?= base_url('js/demo/chart-area-demo.js') ?>"></script>
- <script src="<?= base_url('js/demo/chart-pie-demo.js') ?>"></script>
+ <!-- <script src="<?= base_url('js/demo/chart-area-demo.js') ?>"></script>
+ <script src="<?= base_url('js/demo/chart-pie-demo.js') ?>"></script> -->
 
  <!-- Datatable -->
  <script src="<?= base_url('vendor/datatables/jquery.dataTables.min.js') ?>"></script>
  <script src="<?= base_url('vendor/datatables/dataTables.bootstrap4.min.js') ?>"></script>
  <script src="<?= base_url('js/datepicker/bootstrap-datepicker.min.js') ?>"></script>
+ <script src="https://cdn.ckeditor.com/ckeditor5/24.0.0/classic/ckeditor.js"></script>
 
  <script src="<?= base_url('js/main.js') ?>"></script>
 
@@ -29,5 +30,6 @@
              autoclose: true,
              todayHighlight: true,
          });
+
      });
  </script>
