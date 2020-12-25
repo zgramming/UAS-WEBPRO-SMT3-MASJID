80c6de3329e27a2ace063ff6176887873684f705
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-12-25 19:48:46 +0700

    create landing controller

--- application/controllers/frontend/F_home.php
+++ application/controllers/frontend/F_home.php
@@ -3,36 +3,73 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class F_home extends CI_Controller
 {
-    protected $_table = "products";
-    protected $_tableCategory = "product_category";
-    protected $_tableUnit = "product_unit";
 
+    protected $_tblMosque = "mosque";
+    protected $_tblKhutbah = "khutbah";
+    protected $tblNews = "news";
+    protected $tblInventory = "inventory";
+    protected $tblInventoryCategory = "inventory_category";
+    protected $tblSuggestion = "suggestion";
 
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('admin/a_product_model','productModel');
-        
+        $this->load->model('admin/a_khutbah_model', 'KhutbahModel');
+        $this->load->model('admin/a_management_model', 'ManagementModel');
+        $this->load->model('admin/a_inventory_model', 'InventoryModel');
     }
 
-    public function index()
+    function index()
     {
-        $data['categories'] = $this->db
-            ->order_by('order_position')
-            ->get($this->_tableCategory)
-            ->result();
-        $data['units'] = $this->db->get($this->_tableUnit)->result();
-        $data['products'] = $this->db->get($this->_table)->result();
-
-        $this->template_frontend->display('frontend/home', $data);
+        $data['mosque'] = $this->db->get($this->_tblMosque, 1)->row();
+        $data['khutbahs'] = $this->KhutbahModel->getKhutbahByJoin(null, 'khutbah.date');
+        $data['managements'] = $this->ManagementModel->getManagementByJoin();
+        $data['news'] = $this->db->get($this->tblNews)->result();
+        $this->template_frontend->view("frontend/home", $data, "Beranda");
     }
 
-    public function detail()
+    function khutbah()
     {
-        // $data['product'] = $this->db->get_where($this->_table, ['id_product' => $this->uri->segment(2)])->row();
-        $data['product'] = $this->productModel->getProductWithJoin($this->uri->segment(2));
-        $data['products'] = $this->db->get_where($this->_table, ['id_product !=' => $this->uri->segment(2)])->result();
-        $this->template_frontend->display('frontend/detail', $data);
+        $data['khutbahs'] = $this->KhutbahModel->getKhutbahByJoin(["khutbah.id !=", $this->uri->segment(2)], 'khutbah.date');
+        $data['khutbah'] = $this->KhutbahModel->getKhutbahById($this->uri->segment(2));
+        $this->template_frontend->view("frontend/detail_khutbah", $data, "Khutbah");
+    }
+
+    function news()
+    {
+        $data['news'] = $this->db->get_where($this->tblNews, ['id' => $this->uri->segment(2)])->row();
+        $data['anotherNews'] = $this->db->get_where($this->tblNews, ['id !=' => $this->uri->segment(2)])->result();
+
+        $this->template_frontend->view("frontend/detail_news", $data, "Berita / Agenda");
+    }
+
+    function inventory()
+    {
+        $data['inventories']  = $this->InventoryModel->getInventoryByJoin();
+        $data['categoryInventories'] = $this->db->get($this->tblInventoryCategory)->result();;
+        $this->template_frontend->view("frontend/inventory", $data, "Inventory");
+    }
+
+    function addSuggestion()
+    {
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+        $this->form_validation->set_rules('suggestion', 'Saran & Masukkan', 'required');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->output->set_status_header('400');
+            $this->data['message'] = validation_errors();
+        } else {
+            $data = [
+                "email" => $this->input->post('email'),
+                "suggestion" => $this->input->post('suggestion'),
+                "created_date" => date('Y-m-d H:i:s')
+            ];
+            $this->db->insert($this->tblSuggestion, $data);
+
+            $this->data['message'] = "Berhasil mengirimkan masukkan";
+        }
+
+        echo json_encode($this->data);
     }
 }
 
