3870f0e9601554eb4f2b7213e7396b75d4064bbd
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-01-01 10:37:18 +0700

    hide dashboard menu sidebar

--- application/views/template/admin/sidebar.php
+++ application/views/template/admin/sidebar.php
@@ -13,11 +13,11 @@
      <hr class="sidebar-divider my-0">
 
      <!-- Nav Item - Dashboard -->
-     <li class="nav-item active">
+     <!-- <li class="nav-item active">
          <a class="nav-link" href="<?= base_url('admin/dashboard') ?>">
              <i class="fas fa-fw fa-tachometer-alt"></i>
              <span>Dashboard</span></a>
-     </li>
+     </li> -->
 
      <!-- Divider -->
      <hr class="sidebar-divider">
@@ -26,25 +26,6 @@
      <div class="sidebar-heading">
          Menu Utama
      </div>
-
-     <!-- Nav Item - Pages Collapse Menu -->
-     <!-- <li class="nav-item">
-         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-             <i class="fas fa-cart-arrow-down"></i>
-             <span>Produk</span>
-         </a>
-         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-             <div class="bg-white py-2 collapse-inner rounded">
-                 <h6 class="collapse-header">Daftar Menu : </h6>
-                 <a class="collapse-item" href="<?= base_url('admin/product') ?>">Produk</a>
-                 <a class="collapse-item" href="<?= base_url('admin/category') ?>">Kategori</a>
-                 <a class="collapse-item" href="<?= base_url('admin/unit') ?>">Unit</a>
-             </div>
-         </div>
-     </li> -->
-
-     <!-- Nav Item - Utilities Collapse Menu -->
-
 
      <li class="nav-item">
          <a class="nav-link" href="<?= base_url('admin/ustadz') ?>">
